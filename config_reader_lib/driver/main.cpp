21070571ceee21c136d7dc693d85971ce304f05c
Zack Hall <hallzac@my.wccc.edu>
2016-03-13 15:52:31 -0400

    Study Group FTW

---
+++ config_reader_lib/driver/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "libraries.h"
+
+using namespace std;
+
+int main() {
+
+    config_reader::read_config("C:/Users/outla/Documents/WCCC/Semester 4/CPT-180/Assignment3/lib/app.cfg");
+    return 0;
+}
