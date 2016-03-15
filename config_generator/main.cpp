d1281e849e02c80f0d0fd48b8ae2a27b2eb20b74
HankofWisdom <wownoob29@gmail.com>
2016-03-15 09:28:05 -0400

    Added in the edit and the Create function

--- config_generator/main.cpp
+++ config_generator/main.cpp
@@ -1,8 +1,22 @@
 #include "structures_header.h"
 // Project should read the Config into the struct then Output the Struct to the file
 int main() {
-    string s = "C:\\Users\\Hanko\\Documents\\C++ Files\\Assignment3\\config1.txt";
-    writeConfig(s);
+    string selection = "";
+    cout << "What would you like to do? Create , Edit , or Read :";
+    cin >> selection;
+    if (selection == "Create")
+    {
+        writeConfig("C:\\Users\\Hanko\\Documents\\C++ Files\\Assignment3\\config.txt" , create_config());
+    }else if (selection == "Edit")
+    {
+        writeConfig("C:\\Users\\Hanko\\Documents\\C++ Files\\Assignment3\\config.txt" ,
+                    edit_config("C:\\Users\\Hanko\\Documents\\C++ Files\\Assignment3\\config.txt", selection));
+    }else if (selection == "Read")
+    {
+        writeConfig("C:\\Users\\Hanko\\Documents\\C++ Files\\Assignment3\\config.txt" ,
+                    read_config("C:\\Users\\Hanko\\Documents\\C++ Files\\Assignment3\\config1.txt"));
+    }
+
     return 0;
 }
 int size_finder(string path)
@@ -35,10 +49,82 @@ configStruct read_config(string config_file_path) {
 return info;
     }
 }
-bool writeConfig(string path)
+
+configStruct create_config() {
+    configStruct create;
+
+        cout <<"Create Config\n";
+        cout << "Enter your first name , Last name email , password , and timezone ; Hit Enter after each input :";
+        for (int i = 0; i < 5; ++i) {
+           if (i == 0)
+           {
+               cout <<"Enter yor first name : \n";
+               std::cin >>create.first_name;
+           }else if (i == 1)
+            {
+                cout <<"Enter your Last name : \n";
+                std::cin >> create.last_name;
+            }else if (i == 2)
+           {
+               cout <<"Enter your Email : \n";
+               std::cin >> create.email;
+           }else if (i == 3)
+           {
+               cout <<"Enter your Passcode : \n";
+               std::cin >> create.password;
+           }else
+           {
+               cout <<"Enter your timezone : \n";
+               std::cin >>create.timezone;
+           }
+
+        }
+        return create;
+
+}
+configStruct edit_config(string f , string choice) {
+    configStruct edit;
+    fstream file(f);
+    string selection = "";
+    cout <<"edit Config\n";
+    if(file.is_open()){
+    if(choice == "Edit")
+    {
+        cout <<"What would you like to Edit: \n";
+        std::cin >>selection;
+        while (selection != "Done") {
+        if (selection == "First Name")
+        {
+            cout <<"Enter your first name : \n";
+            std::cin >>edit.first_name;
+        }else if (selection == "Last Name")
+        {
+            cout <<"Enter your Last name : \n";
+            std::cin >> edit.last_name;
+        }else if (selection == "Email")
+        {
+            cout <<"Enter your Email : \n";
+            std::cin >> edit.email;
+        }else if (selection == "Passcode")
+        {
+            cout <<"Enter your Passcode : \n";
+            std::cin >> edit.password;
+        }else if (selection == "Timezone")
+        {
+            cout <<"Enter your timezone : \n";
+            std::cin >>edit.timezone;
+        }
+
+    }
+    }
+    }
+    return edit;
+
+}
+
+bool writeConfig(string path , configStruct data)
 {
     fstream file(path);
-    configStruct data = read_config("C:\\Users\\Hanko\\Documents\\C++ Files\\Assignment3\\config.txt");
     if(!file)
     {
         cout <<"File Didn't Work\n";
--- config_generator/structures_header.h
+++ config_generator/structures_header.h
@@ -7,10 +7,13 @@ struct configStruct
     string first_name ,last_name, email , password , timezone;
 
 };
-bool writeConfig(string path);
+bool writeConfig(string path , configStruct data);
 int size_finder(string path);
 using std::cout;
+using std::cin;
 using std::string;
 using std::fstream;
 #include <fstream>
 configStruct read_config(string config_file_path) ;
+configStruct create_config();
+configStruct edit_config(string file , string choice);
