2008d906b2cec82fc4b4402a30f8f6bb372cdd4a
Tim Connelly <connellyt@my.wccc.edu>
2016-03-15 20:14:14 -0400

    [WIP] Just the init stuff, putting the other stuff up shortly.

---
+++ Assignment _5/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+
+//#include './library.h"
+
+const std::string CONFIG = "C:/Users/Tim/ClionProjects/Assignment_2_File_Generator/config.txt";
+const std::string EDIT = "C:/Users/Tim/ClionProjects/Assignment_2_File_Generator/edit.txt";
+
+int main(int argc, char *argv[]) {
+
+
+    if (static_cast<std::string>(argv[1]) == "init") {
+        std::ofstream myfile;
+        myfile.open(CONFIG);
+
+        std::string first_and_last = "", email = "", cypher = "", timezone;
+
+        std::cout << "Enter your first and last name, please" << std::endl;
+        std::getline(std::cin, first_and_last);
+
+        while (first_and_last == "") {
+            std::cout << "Please enter your first and last name to continue" << std::endl;
+            std::getline(std::cin, first_and_last);
+        }
+        myfile << first_and_last << std::endl;
+
+        std::cout << "Enter your email address, please" << std::endl;
+        std::getline(std::cin, email);
+
+        while (email == "") {
+            std::cout << "Please enter your email to continue" << std::endl;
+            std::getline(std::cin, email);
+        }
+        myfile << email << std::endl;
+
+        std::cout << "Enter your unique cypher, please" << std::endl;
+        std::getline(std::cin, cypher);
+
+        while (cypher == "") {
+            std::cout << "Please enter your unique cypher to continue" << std::endl;
+            std::getline(std::cin, cypher);
+        }
+        myfile << cypher << std::endl;
+
+        std::cout << "Enter your timezone offset, please" << std::endl;
+        std::getline(std::cin, timezone);
+
+
+        while (timezone == "") {
+            std::cout << "Please enter your timezone to continue" << std::endl;
+            std::getline(std::cin, timezone);
+        }
+        myfile << timezone << std::endl;
+
+
+        myfile.close();
+    }
+    return 0;
+
+}
---
+++ Assignment _5/main.h
@@ -0,0 +1,16 @@
+//
+// Created by Tim on 3/13/2016.
+//
+
+#ifndef ASSIGNMENT_5_MAIN_H
+#define ASSIGNMENT_5_MAIN_H
+
+#endif //ASSIGNMENT_5_MAIN_H
+
+//struct config{
+  //  std::string first;
+    //std::string last;
+//    std::string cypher;
+  //  std::string timzone;
+    //std::string email;
+//}
