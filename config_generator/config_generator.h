bd9c1d2444c417e18e42b15e5845ecde0a6718a7
Jonathan Chishko <Chishkoj@my.wccc.edu>
2016-03-18 08:03:18 -0400

    Assignment 5

---
+++ config_generator/config_generator.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+using std::string
+
+struct config{
+    string name;
+    string email;
+    string timezone;
+    string cypher;
+    string knowrecipientsfile;
+};
